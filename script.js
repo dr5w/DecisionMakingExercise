39505acece8faaf9e7bcd9da7f3db328844a4ddb
dr5w <111304827+dr5w@users.noreply.github.com>
2023-02-14 10:28:14 -0600

    Completed assignment

---
+++ script.js
@@ -0,0 +1,19 @@
+//1a. 
+let randomNum = Math.floor(Math.random() * 5 + 1);
+console.log(randomNum);
+//1b. 
+if (randomNum >= 4)
+    console.log("Greater than or equal to 4");
+else if (randomNum == 3 || randomNum == 2)
+    console.log("Equal to 2 or 3");
+else
+console.log("Equal to 1");
+//1c. 
+if (randomNum != 3)
+    console.log("Not equal to 3");
+//1d. 
+if (randomNum != 3 & randomNum != 5)
+    console.log("NOT equal to 3 AND not equal to 5");
+//1e. 
+if (randomNum == 2 || randomNum == 4)
+    console.log("Equal to 2 OR equal to 4");
